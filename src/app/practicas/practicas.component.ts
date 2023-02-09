4776b45d0f347fb38157b02118e263d3706c9cc0
RobertoUAT <a2061410280@alumnos.uat.edu.mx>
2023-02-09 11:13:14 -0600

    9 de febrero 2023
    trabajos realizados en clase de programacion

--- src/app/practicas/practicas.component.ts
+++ src/app/practicas/practicas.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class PracticasComponent {
 
+  //*delaracion de variables*/
+texto!: string
+nombre!: string
+apeido!: string
+nombrecompleto!: string
+
+
+  //*este es un metodo*/
+  ngOnInit(){
+      //*asignacion de valores*/
+    this.texto = "Hola Mundo!"
+    this.nombre = "Roberto "
+    this.apeido = "Pacheco"
+    this.nombrecompleto = this.nombre + this.apeido
+
+      //*ACTIVIDAD 1*/
+      //*costrar en consola*/
+    console.log(this.texto)
+
+      //*mostrar en alerta*/
+    alert(this.texto)
+  }
+
 }
