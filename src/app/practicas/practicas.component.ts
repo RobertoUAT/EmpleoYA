2fd429c948900efda5d863acff952ad9de236f2f
RobertoUAT <a2061410280@alumnos.uat.edu.mx>
2023-02-10 12:39:58 -0600

    10 febrero 2023
    practicas (sumar)

--- src/app/practicas/practicas.component.ts
+++ src/app/practicas/practicas.component.ts
@@ -12,7 +12,18 @@ texto!: string
 nombre!: string
 apeido!: string
 nombrecompleto!: string
+numero1!: number
+numero2!: number
+resultado!: number
+resultado2!: number
+valor1!: number
+valor2!: number
+resultado3!: number
 
+ //*este es un metodo*/
+ calcular(){
+this.resultado3 = this.valor1 + this.valor2
+ }
 
   //*este es un metodo*/
   ngOnInit(){
@@ -22,10 +33,19 @@ nombrecompleto!: string
     this.apeido = "Pacheco"
     this.nombrecompleto = this.nombre + this.apeido
 
+    //*ACTIVIDAD 3*/
+    this.numero1 = 10
+    this.numero2 = 5
+    this.resultado = this.numero1 + this.numero2
+
+     //*ACTIVIDAD 4*/
+    this.resultado2 =this.numero1 * this.numero2    
+
       //*ACTIVIDAD 1*/
       //*costrar en consola*/
     console.log(this.texto)
 
+    //*ACTIVIDAD 2*/
       //*mostrar en alerta*/
     alert(this.texto)
   }
